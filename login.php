a1004825fe30969796aaab71620bca0e009f0967
User-HP\User <andreaskrisandy>
2017-10-25 15:35:48 +0700

    demploy login

---
+++ login.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+<title>Datawarehosue</title>
+
+<link href="main/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="main/css/londinium-theme.css" rel="stylesheet" type="text/css">
+<link href="main/css/styles.css" rel="stylesheet" type="text/css">
+<link href="main/css/icons.css" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+
+<script type="text/javascript" src="mian/js/plugins/charts/sparkline.min.js"></script>
+
+<script type="text/javascript" src="main/js/plugins/forms/uniform.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/select2.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/inputmask.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/autosize.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/inputlimit.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/listbox.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/multiselect.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/validate.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/tags.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/switch.min.js"></script>
+
+<script type="text/javascript" src="main/js/plugins/forms/uploader/plupload.full.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/uploader/plupload.queue.min.js"></script>
+
+<script type="text/javascript" src="main/js/plugins/forms/wysihtml5/wysihtml5.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/forms/wysihtml5/toolbar.js"></script>
+
+<script type="text/javascript" src="main/js/plugins/interface/daterangepicker.js"></script>
+<script type="text/javascript" src="main/js/plugins/interface/fancybox.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/interface/moment.js"></script>
+<script type="text/javascript" src="main/js/plugins/interface/jgrowl.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/interface/datatables.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/interface/colorpicker.js"></script>
+<script type="text/javascript" src="main/js/plugins/interface/fullcalendar.min.js"></script>
+<script type="text/javascript" src="main/js/plugins/interface/timepicker.min.js"></script>
+
+<script type="text/javascript" src="main/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="main/js/application.js"></script>
+
+</head>
+
+<body class="full-width page-condensed">
+
+	<!-- Navbar -->
+	<div class="navbar navbar-inverse" role="navigation">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#"><img src="images/logo.png" alt="Datawarehouse"></a>
+		</div>
+	</div>
+	<!-- /navbar -->
+
+
+	<!-- Login wrapper -->
+	<div class="login-wrapper">
+    	<form action="#" role="form">
+			<div class="popup-header">
+				<a href="#" class="pull-left"><i class="icon-user-plus"></i></a>
+				<span class="text-semibold">User Masuk</span>
+			</div>
+			<div class="well">
+				<div class="form-group has-feedback">
+					<label>Username</label>
+					<input type="text" class="form-control" placeholder="Username">
+					<i class="icon-users form-control-feedback"></i>
+				</div>
+
+				<div class="form-group has-feedback">
+					<label>Password</label>
+					<input type="password" class="form-control" placeholder="Password">
+					<i class="icon-lock form-control-feedback"></i>
+				</div>
+
+				<div class="row form-actions">
+					<div class="col-xs-6">
+						<button type="submit" class="btn btn-warning pull-right"><i class="icon-menu2"></i> Sign in</button>
+					</div>
+				</div>
+			</div>
+    	</form>
+	</div>
+	<!-- /login wrapper -->
+</body>
+</html>
